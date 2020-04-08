c375f2654beac7dabaa6b083ce6289460f04cfa2
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2020-04-08 14:44:52 -0500

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,7 @@ module.exports = {
     titleShort: `Julián Duque - MNTD LLC`,
     description: `Hello, my name is Julián Duque, I'm from Medellín, Colombia 🇨🇴 but currently living in St. Petersburg, FL (Tampa Bay, FL) 🇺🇸.
     I'm a community leader, public speaker, JavaScript/Node.js evangelist, and an official Node.js collaborator. I work at NodeSource as a Solutions Architect and currently organizing JSConf and NodeConf Colombia, also helping organize Suncoast.js in St. Petersburg, FL, and MedellinJS, the largest JavaScript user group in Colombia with 4500+ registered members.`,
-    author: `@fluxiongc`,
-    image:
-      "https://raw.githubusercontent.com/nodeconf/colombia/d5eaf4bba41a10a7a02f40ff28320207046c18a7/images/social.png",
-    url: "https://julianduque.co/"
+    author: `@fluxiongc`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -36,13 +33,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
-    {
-      resolve: "gatsby-plugin-mailchimp",
-      options: {
-        endpoint:
-          "https://nodeconf.us18.list-manage.com/subscribe/post?u=33c93ca7e73c6c27cd0325b9f&amp;id=e71faec96c"
-      }
-    },
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
