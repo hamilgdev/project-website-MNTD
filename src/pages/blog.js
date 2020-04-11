f3f28888ce8b908aa6d61e9b4a2f227493696893
miltoncodeyt <miltoncodeyt@gmail.com>
2020-04-11 16:02:56 -0500

    add navigation on pages

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import "./styles.scss";
+
+class BlogPage extends React.Component {
+ 
+  render() {
+    return <Layout></Layout>
+  }
+}
+
+export default BlogPage;
---
+++ src/pages/mntd.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import "./styles.scss";
+
+class MNTDPage extends React.Component {
+ 
+  render() {
+    return <Layout></Layout>
+  }
+}
+
+export default MNTDPage;
