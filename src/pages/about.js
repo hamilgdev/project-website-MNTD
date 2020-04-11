8862b71fa87d20bba6b450d08ee3510d592c9075
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2020-04-11 15:24:08 -0500

    page about

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+import About from "../components/About";
+
+export default class about extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <About />
+      </Layout>
+    );
+  }
+}
