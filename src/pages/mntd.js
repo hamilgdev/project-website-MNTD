7afa765620bd04ad018a29408e55f9222f243609
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2020-04-13 18:37:57 -0500

    section mntd

--- src/pages/mntd.js
+++ src/pages/mntd.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import Mntd from "../components/Mntd";
 import "./styles.scss";
 
 class MNTDPage extends React.Component {
- 
   render() {
-    return <Layout></Layout>
+    return (
+      <Layout>
+        <Mntd />
+      </Layout>
+    );
   }
 }
 
