f916d85452de896538d8de46ad426ca852ed7ae7
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2020-04-28 00:05:22 -0500

    fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#2E6B41`,
         theme_color: `#2E6B41`,
         display: `minimal-ui`,
-        icon: `src/images/logotipo.png` // This path is relative to the root of the site.
+        icon: `src/images/Logotipo_mntd.png` // This path is relative to the root of the site.
       }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
